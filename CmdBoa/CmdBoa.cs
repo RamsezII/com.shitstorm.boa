2bf0211144a2dbd8e558c0629b59bee42c1277f5
ram16 <thomass.montaine@gmail.com>
2025-04-06 21:27:19 +0200

    Update CmdBoa.cs

--- CmdBoa/CmdBoa.cs
+++ CmdBoa/CmdBoa.cs
@@ -5,16 +5,17 @@ namespace _BOA_
 {
     static internal class CmdBoa
     {
-        static readonly Command instance = new(manual: new("write your own shitstorm scripts :)"));
+        static readonly Command instance = new("boa", manual: new("write your own shitstorm scripts :)"));
 
         //--------------------------------------------------------------------------------------------------------------
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         static void OnAfterSceneLoad()
         {
-            Command.cmd_root_shell.AddCommand("boa", instance);
+            Command.cmd_root_shell.AddCommand(instance);
 
-            instance.AddCommand("execute-script", new(
+            instance.AddCommand(new(
+                "execute-script",
                 manual: new("execute script at path <path>"),
                 args: exe =>
                 {
