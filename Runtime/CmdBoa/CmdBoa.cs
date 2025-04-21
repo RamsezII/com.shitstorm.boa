a86232c3810118c05116508f01d57c7687f6c070
ram16 <thomass.montaine@gmail.com>
2025-04-21 17:17:03 +0200

    vers basé sur command line

--- Runtime/CmdBoa/CmdBoa.cs
+++ Runtime/CmdBoa/CmdBoa.cs
@@ -1,5 +1,6 @@
 ﻿using _COBRA_;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace _BOA_
@@ -11,23 +12,38 @@ namespace _BOA_
         {
             Command domain_boa = Command.static_domain.AddDomain(
                 "boa",
-                manual: new("write your own shitstorm scripts :)")
+                manual: new("create and run your own shitstorm scripts :)")
                 );
 
             domain_boa.AddRoutine(
-                "execute-script",
+                "run",
                 manual: new("execute script at path <path>"),
                 min_args: 1,
                 args: exe =>
                 {
-                    if (exe.line.TryReadArgument(out string arg, out _))
-                        exe.args.Add(arg);
+                    if (exe.line.TryReadArgument(out string script_path, out _, strict: true, path_mode: PATH_FLAGS.FILE))
+                        exe.args.Add(script_path);
                 },
-                routine: ERoutine);
+                routine: ERun);
 
-            static IEnumerator<CMD_STATUS> ERoutine(Command.Executor exe)
+            static IEnumerator<CMD_STATUS> ERun(Command.Executor exe)
             {
-                yield break;
+                string script_path = (string)exe.args[0];
+                script_path = exe.shell.PathCheck(script_path, PathModes.ForceFull);
+
+                string script = File.ReadAllText(script_path);
+
+                while (true)
+                {
+                    int read_i = 0;
+                    foreach (string script_line in script.IterateThroughData_str())
+                    {
+                        Command.Line cmd_line = new(script_line, exe.line.signal, exe.shell, cursor_i: int.MaxValue);
+                        exe.shell.PropagateLine(cmd_line);
+                        read_i += script_line.Length;
+                    }
+                    yield return new CMD_STATUS(CMD_STATES.BLOCKING);
+                }
             }
         }
     }
