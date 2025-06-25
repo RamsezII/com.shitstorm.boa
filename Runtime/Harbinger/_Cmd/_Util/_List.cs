ce47a8f262417dc4af32cb9136e24770bb3a0d8c
ram16 <thomass.montaine@gmail.com>
2025-06-25 19:26:50 +0200

    append method

--- Runtime/Harbinger/_Cmd/_Util/_List.cs
+++ Runtime/Harbinger/_Cmd/_Util/_List.cs
@@ -30,6 +30,40 @@ namespace _BOA_
                         stack: routines
                         );
                 }));
+
+            AddContract(new("append",
+                min_args: 1,
+                args: static exe =>
+                {
+                    if (exe.harbinger.TryParseExpression(exe.reader, exe.scope, true, out var expr_list))
+                        exe.args.Add(expr_list);
+                    else
+                        exe.reader.Stderr("expected expression of type List.");
+
+                    if (exe.pipe_previous == null)
+                        if (exe.harbinger.TryParseExpression(exe.reader, exe.scope, true, out var expr_item))
+                            exe.arg_0 = expr_item;
+                        else
+                            exe.reader.Stderr("expeted item to add to the list.");
+                },
+                routine: static exe =>
+                {
+                    ExpressionExecutor expr_list = (ExpressionExecutor)exe.args[0];
+                    using var expr_routine = expr_list.EExecute();
+                    using var expr_item = exe.arg_0.EExecute();
+
+                    return Executor.EExecute(
+                        after_execution: null,
+                        modify_output: data =>
+                        {
+                            List<object> list = (List<object>)expr_routine.Current.output;
+                            object item = expr_item.Current.output;
+                            list.Add(item);
+                            return list;
+                        },
+                        expr_routine,
+                        expr_item);
+                }));
         }
     }
 }
