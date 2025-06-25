550dbed451fe58efcc1cab6c39093fe5956f4b09
ram16 <thomass.montaine@gmail.com>
2025-06-25 19:08:05 +0200

    yes

---
+++ Runtime/Harbinger/_Cmd/_Files/_Path.cs
@@ -0,0 +1,40 @@
+﻿using _UTIL_;
+using UnityEngine;
+
+namespace _BOA_
+{
+    partial class Harbinger
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void InitCmd_Path()
+        {
+            AddContract(new("path",
+                min_args: 1,
+                opts: static exe =>
+                {
+                    FS_TYPES type = FS_TYPES.BOTH;
+                    if (exe.reader.TryReadChar_matches_out(out char opt, true, "fd"))
+                        switch (opt)
+                        {
+                            case 'f':
+                                type = FS_TYPES.FILE;
+                                break;
+                            case 'd':
+                                type = FS_TYPES.DIRECTORY;
+                                break;
+                        }
+                    exe.opts.Add(nameof(FS_TYPES), type);
+                },
+                args: static exe =>
+                {
+                    FS_TYPES type = (FS_TYPES)exe.opts[nameof(FS_TYPES)];
+                    if (exe.harbinger.TryParsePath(exe.reader, type, true, out string path))
+                        exe.args.Add(path);
+                    else
+                        exe.reader.Stderr($"Expects expression of type path.");
+                },
+                function: static exe => exe.harbinger.PathCheck((string)exe.args[0], PathModes.TryMaintain, false, false, out _, out _)
+                ));
+        }
+    }
+}
---
+++ Runtime/Harbinger/_Cmd/_Files/_dex.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace _BOA_
+{
+    partial class Harbinger
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void InitCmd_FS_Exists()
+        {
+            AddContract(new("directory-exists",
+                min_args: 1,
+                outputs_if_end_of_instruction: true,
+                args: static exe =>
+                {
+                    if (exe.pipe_previous == null)
+                        if (exe.harbinger.TryParseExpression(exe.reader, exe.scope, true, out var expr))
+                            exe.arg_0 = expr;
+                        else
+                            exe.reader.Stderr($"expected path expression.");
+                },
+                routine: static exe =>
+                {
+                    return Executor.EExecute(
+                        after_execution: null,
+                        modify_output: data =>
+                        {
+                            string path = (string)data;
+                            path = exe.harbinger.PathCheck(path, PathModes.ForceFull, false, false, out _, out _);
+                            return Directory.Exists(path);
+                        },
+                        exe.arg_0.EExecute());
+                }),
+                "dex");
+
+            AddContract(new("file-exists",
+                min_args: 1,
+                outputs_if_end_of_instruction: true,
+                args: static exe =>
+                {
+                    if (exe.pipe_previous == null)
+                        if (exe.harbinger.TryParseExpression(exe.reader, exe.scope, true, out var expr))
+                            exe.arg_0 = expr;
+                        else
+                            exe.reader.Stderr($"expected path expression.");
+                },
+                routine: static exe =>
+                {
+                    return Executor.EExecute(
+                        after_execution: null,
+                        modify_output: data =>
+                        {
+                            string path = (string)data;
+                            path = exe.harbinger.PathCheck(path, PathModes.ForceFull, false, false, out _, out _);
+                            return File.Exists(path);
+                        },
+                        exe.arg_0.EExecute());
+                }),
+                "fex");
+        }
+    }
+}
